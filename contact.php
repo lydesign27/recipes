97a8176222d34b40ffcb3454679187ac6bdb293f
lydesign27 <leidiane@hotmail.fr>
2026-02-18 15:20:47 +0100

    formulaire de contact

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/functions.php');
  <div class="container">
         <?php include(__DIR__ . '/includes/header.php'); ?>
         <h1>Contactez nous</h1>
-        <form action="submit_contact.php" method="GET">
+        <form action="submit_contact.php" method="POST">
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" class="form-control" id="email" name="email" aria-describedby="email-help">
@@ -15,6 +15,11 @@ require_once(__DIR__ . '/functions.php');
                 <label for="message" class="form-label">Votre message</label>
                 <textarea class="form-control" placeholder="Exprimez vous" id="message" name="message"></textarea>
             </div>
+             <!-- Ajout champ d'upload ! -->
+    <div class="mb-3">
+        <label for="screenshot" class="form-label">Votre capture d'écran</label>
+        <input type="file" class="form-control" id="screenshot" name="screenshot" />
+    </div>
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </form>
         <br />
@@ -22,5 +27,5 @@ require_once(__DIR__ . '/functions.php');
 
 
 
-    <?php include(__DIR__ . '/includes/footer.php'); ?>
+    <?php require_once(__DIR__ . '/includes/footer.php'); ?>
 
